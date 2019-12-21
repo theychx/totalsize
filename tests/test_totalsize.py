ad9ab0f184dc22709704c70f7ea5e61c05574f53
theychx <theychx@users.noreply.github.com>
2019-12-21 21:05:26 +0100

    Implement more info option (#10)
    * Implement more info option
    
    * Fix tests
    
    * Bump version: 0.1.4 → 0.2.0

--- tests/test_totalsize.py
+++ tests/test_totalsize.py
@@ -1,29 +1,32 @@
 import sys
 import unittest
 
-from totalsize.total import get_totalsize
+from totalsize.total import Playlist
 
 
 class TestTotal(unittest.TestCase):
     def test_get_totalsize_yt_channel(self):
-        playlist = get_totalsize("https://www.youtube.com/channel/UCvAUb8YbRyXz_l9CXptKoQA/", "18")
-        self.assertEqual(playlist.total_sum, 425674114)
+        playlist = Playlist("https://www.youtube.com/channel/UCvAUb8YbRyXz_l9CXptKoQA/", "18")
+        playlist.accum_info()
+        self.assertEqual(playlist.totals.size, 425674114)
         self.assertEqual(playlist.number_of_media, 4)
         self.assertEqual(playlist.number_of_media_inacc, 0)
         self.assertEqual(playlist.number_of_media_nosize, 0)
 
     def test_get_totalsize_yt_playlist(self):
-        playlist = get_totalsize(
+        playlist = Playlist(
             "https://www.youtube.com/watch?v=KIHBpp34JkA&list=PLGx22rG4Cm6dEFvkjmdSRpulz6l0M-g2u", "18"
         )
-        self.assertEqual(playlist.total_sum, 197941592)
+        playlist.accum_info()
+        self.assertEqual(playlist.totals.size, 197941592)
         self.assertEqual(playlist.number_of_media, 3)
         self.assertEqual(playlist.number_of_media_inacc, 0)
         self.assertEqual(playlist.number_of_media_nosize, 0)
 
     def test_get_totalsize_yt_video(self):
-        playlist = get_totalsize("https://www.youtube.com/watch?v=KIHBpp34JkA", "18")
-        self.assertEqual(playlist.total_sum, 69574304)
+        playlist = Playlist("https://www.youtube.com/watch?v=KIHBpp34JkA", "18")
+        playlist.accum_info()
+        self.assertEqual(playlist.totals.size, 69574304)
         self.assertEqual(playlist.number_of_media, 1)
         self.assertEqual(playlist.number_of_media_inacc, 0)
         self.assertEqual(playlist.number_of_media_nosize, 0)
--- totalsize/total.py
+++ totalsize/total.py
@@ -3,6 +3,7 @@ import math
 import re
 import sys
 import tempfile
+import time
 from pathlib import Path
 
 import youtube_dl
@@ -12,13 +13,28 @@ FORMAT_DOC_URL = "https://github.com/ytdl-org/youtube-dl#format-selection"
 FRAGMENTS_REGEX = re.compile(r"range/[\d]+-([\d]+)$")
 TEMPPATH = Path(tempfile.gettempdir(), "totalsize", "fragment")
 YTDL_OPTS = {"quiet": True, "no_warnings": True, "outtmpl": str(TEMPPATH)}
+MULT_NAMES_BTS = ("B", "KB", "MB", "GB", "TB", "PB")
+MULT_NAMES_DEC = ("", "K", "M", "B")
 
-LINE_LENGTH = 75
-TITLE_FIELD_SIZE = LINE_LENGTH - 12
-REPORT_TEMPLATE_1 = "{title:<{field_size}}{msg:>12}"
-REPORT_TEMPLATE_2 = "{title:<{field_size}} {inaccurate}{size[0]:>7.2f} {size[1]:<2}"
-PAD = "-" * LINE_LENGTH
-TOTAL_SIZE_TXT = "Total size of all media with reported size"
+TXT_FIELD_SIZE = 60
+MSG_FIELD_SIZE = 12
+MORE_FIELD_SIZE = 48
+REPORT_STRING = "{txt:<60}{msg:>12}"
+SIZE_STRING = "{0:>7.1f} {1}"
+SIZE_STRING_NO_MULT = "{0:>7}"
+MORE_STRING = "{duration:>12}{views:>9}{likes:>9}{dislikes:>9}{likes_percentage:>9}"
+LEGACY = {"txt": "", "msg": "Size"}
+MORE_LEGACY = {
+    "duration": "Duration",
+    "views": "Views",
+    "likes": "Likes",
+    "dislikes": "Dislikes",
+    "likes_percentage": "L/D%",
+}
+PAD_CHAR = "-"
+PAD = PAD_CHAR * (TXT_FIELD_SIZE + MSG_FIELD_SIZE)
+MPAD = PAD_CHAR * (TXT_FIELD_SIZE + MSG_FIELD_SIZE + MORE_FIELD_SIZE)
+TOTALS = "Totals"
 TOTAL_MEDIA_TXT = "Total number of media files"
 TOTAL_INACC_TXT = "Total number of media files with inaccurate reported size"
 TOTAL_NO_SIZE_TXT = "Total number of media files with no reported size"
@@ -31,6 +47,71 @@ class ResourceNotFoundError(Exception):
 
 class FormatSelectionError(Exception):
     pass
+
+
+class Entry:
+    def __init__(self, title, inaccurate, size, duration, views, likes, dislikes):
+        self.title = title
+        self.inaccurate = inaccurate
+        self.size = size
+        self.duration = duration
+        self.views = views
+        self.likes = likes
+        self.dislikes = dislikes
+
+    @property
+    def truncated_title(self):
+        title = self.title
+        if title is None:
+            return None
+        return title[: TXT_FIELD_SIZE - 3] + "..." if len(title) > TXT_FIELD_SIZE else title
+
+    @property
+    def likes_percentage(self):
+        if self.likes is None or self.dislikes is None or self.likes == self.dislikes == 0:
+            return None
+        if self.likes == 0:
+            return 0
+        return (self.likes / (self.likes + self.dislikes)) * 100
+
+    @property
+    def readable_size(self):
+        return self._readable_amount(self.size, byte=True)
+
+    @property
+    def readable_duration(self):
+        return time.strftime("%H:%M:%S", time.gmtime(self.duration)) if self.duration is not None else None
+
+    @property
+    def readable_views(self):
+        return self._readable_amount(self.views)
+
+    @property
+    def readable_likes(self):
+        return self._readable_amount(self.likes)
+
+    @property
+    def readable_dislikes(self):
+        return self._readable_amount(self.dislikes)
+
+    @property
+    def readable_likes_percentage(self):
+        return "{:.1f}%".format(self.likes_percentage) if self.likes_percentage is not None else None
+
+    def _readable_amount(self, amount, byte=False):
+        if amount is None:
+            return None
+        mult = 1024 if byte else 1000
+        mult_names = MULT_NAMES_BTS if byte else MULT_NAMES_DEC
+        if amount == 0:
+            return SIZE_STRING_NO_MULT.format(0)
+
+        ind = int(math.floor(math.log(amount, mult)))
+        pwr = math.pow(mult, ind)
+        mname = mult_names[ind]
+        size = round(amount / pwr, ndigits=1) if pwr > 1 else amount
+        fstr = SIZE_STRING if mname else SIZE_STRING_NO_MULT
+        return fstr.format(size, mname)
 
 
 class Playlist:
@@ -51,7 +132,71 @@ class Playlist:
             raise ResourceNotFoundError
 
         self._medias = preinfo.get("entries") or [preinfo]
-        self.number_of_media = self.number_of_media_inacc = self.number_of_media_nosize = self.total_sum = 0
+        self._entries = []
+
+    @property
+    def totals(self):
+        if not self._entries:
+            return None
+        info = {
+            "title": None,
+            "inaccurate": any(e.inaccurate for e in self._entries),
+            "size": sum(e.size for e in self._entries if e.size),
+            "duration": sum(e.duration for e in self._entries if e.duration),
+            "views": sum(e.views for e in self._entries if e.views),
+            "likes": sum(e.likes for e in self._entries if e.likes),
+            "dislikes": sum(e.dislikes for e in self._entries if e.dislikes),
+        }
+        return Entry(**info)
+
+    @property
+    def number_of_media(self):
+        return len(self._entries)
+
+    @property
+    def number_of_media_inacc(self):
+        return sum(1 for e in self._entries if e.inaccurate)
+
+    @property
+    def number_of_media_nosize(self):
+        return sum(1 for e in self._entries if e.size is None)
+
+    def accum_info(self):
+        for _ in self.gen_info():
+            pass
+
+    def gen_info(self):
+        for media in self._medias:
+            media_info = self._get_media_info(media) or {}
+            inaccurate, size = self._get_size(media_info) if media_info else (False, None)
+            info = {
+                "title": media.get("title"),
+                "inaccurate": inaccurate,
+                "size": size,
+                "duration": media_info.get("duration"),
+                "views": media_info.get("view_count"),
+                "likes": media_info.get("like_count"),
+                "dislikes": media_info.get("dislike_count"),
+            }
+            entry = Entry(**info)
+            self._entries.append(entry)
+            yield entry
+
+    def _get_media_info(self, media):
+        try:
+            return self._ydl.process_ie_result(media, download=False)
+        except (youtube_dl.utils.DownloadError, youtube_dl.utils.ExtractorError):
+            return None
+
+    def _get_size(self, media_info):
+        try:
+            best = next(self._selector(media_info))
+        except StopIteration:
+            raise FormatSelectionError
+        except KeyError:
+            best = media_info
+
+        return self._calc_size(best.get("requested_formats") or [best])
 
     def _calc_size(self, info):
         media_sum = 0
@@ -94,90 +239,71 @@ class Playlist:
                 return (False, None)
         return (inaccurate, media_sum)
 
-    def _get_size(self, info):
-        try:
-            media = self._ydl.process_ie_result(info, download=False)
-        except (youtube_dl.utils.DownloadError, youtube_dl.utils.ExtractorError):
-            return (False, None)
 
-        try:
-            best = next(self._selector(media))
-        except StopIteration:
-            raise FormatSelectionError
-        except KeyError:
-            best = media
-        return self._calc_size(best.get("requested_formats") or [best])
+def print_report_line(entry=None, txt="", msg="", more_info=False, err=False):
+    fstr = REPORT_STRING
+    if entry:
+        txt = txt or entry.truncated_title
+        if not msg and entry.size:
+            inaccurate = "~" if entry.inaccurate else ""
+            msg = inaccurate + entry.readable_size
 
-    def get_info(self):
-        for media in self._medias:
-            inaccurate, size = self._get_size(media)
-            self.number_of_media += 1
-            if inaccurate:
-                self.number_of_media_inacc += 1
-            if size is None:
-                self.number_of_media_nosize += 1
-            else:
-                self.total_sum += size
-            yield (media["title"], inaccurate, size)
+    report_line = {"txt": txt, "msg": msg}
+    if more_info:
+        report_line.update(
+            {
+                "duration": entry.readable_duration or "",
+                "views": entry.readable_views or "",
+                "likes": entry.readable_likes or "",
+                "dislikes": entry.readable_dislikes or "",
+                "likes_percentage": entry.readable_likes_percentage or "",
+            }
+        )
+        fstr += MORE_STRING
+    print(fstr.format(**report_line), file=sys.stderr if err else sys.stdout)
 
 
-def readable_size(size_bytes):
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    ind = int(math.floor(math.log(size_bytes, 1024)))
-    pwr = math.pow(1024, ind)
-    size = round(size_bytes / pwr, 2)
-    return (size, size_name[ind])
+def print_legacy_line(more_info=False):
+    fstr = REPORT_STRING
+    legacy_line = LEGACY
+    if more_info:
+        legacy_line.update(MORE_LEGACY)
+        fstr += MORE_STRING
+    print(fstr.format(**legacy_line))
 
 
-def print_report_line(template, title, size=None, inaccurate=False, msg=None, err=False):
-    report_line = {
-        "title": title[: TITLE_FIELD_SIZE - 3] + "..." if len(title) > TITLE_FIELD_SIZE else title,
-        "field_size": TITLE_FIELD_SIZE,
-        "msg": msg or "",
-        "inaccurate": "~" if inaccurate else " ",
-        "size": readable_size(size) if size else None,
-    }
-    print(template.format(**report_line), file=sys.stderr if err else sys.stdout)
-
-
-def get_totalsize(url, format_filter, report_mode=False):
+def print_report(url, format_filter, more_info=False):
     playlist = Playlist(url, format_filter)
-    info = playlist.get_info()
+    pad = MPAD if more_info else PAD
+
+    print_legacy_line(more_info=more_info)
+    print(pad)
 
     try:
-        for title, inaccurate, size in info:
-            if not report_mode:
-                continue
-            if size is None:
-                print_report_line(REPORT_TEMPLATE_1, title, msg="no size", err=True)
+        for entry in playlist.gen_info():
+            if entry.size is None:
+                print_report_line(entry=entry, msg="no size", more_info=more_info, err=True)
             else:
-                print_report_line(REPORT_TEMPLATE_2, title, size=size, inaccurate=inaccurate)
+                print_report_line(entry=entry, more_info=more_info)
     except KeyboardInterrupt:
-        if report_mode:
-            print_report_line(REPORT_TEMPLATE_1, ABORT_TXT, err=True)
-        else:
-            raise
-    else:
-        if not report_mode:
-            return playlist
+        print_report_line(txt=ABORT_TXT, err=True)
 
-    if playlist.total_sum:
-        print(PAD)
-        print_report_line(
-            REPORT_TEMPLATE_2, TOTAL_SIZE_TXT, size=playlist.total_sum, inaccurate=bool(playlist.number_of_media_inacc)
-        )
+    print(pad)
+    print_legacy_line(more_info=more_info)
+    print(pad)
+    print_report_line(txt=TOTALS, entry=playlist.totals, more_info=more_info)
 
-    print(PAD)
-    print_report_line(
-        REPORT_TEMPLATE_1, TOTAL_MEDIA_TXT, msg=playlist.number_of_media, err=not playlist.number_of_media
-    )
+    print(pad)
+    number_of_media = playlist.number_of_media
+    print_report_line(txt=TOTAL_MEDIA_TXT, msg=number_of_media, err=not number_of_media)
 
-    if playlist.number_of_media_inacc:
-        print_report_line(REPORT_TEMPLATE_1, TOTAL_INACC_TXT, msg=playlist.number_of_media_inacc)
-    if playlist.number_of_media_nosize:
-        print_report_line(REPORT_TEMPLATE_1, TOTAL_NO_SIZE_TXT, msg=playlist.number_of_media_nosize, err=True)
+    number_of_media_inacc = playlist.number_of_media_inacc
+    if number_of_media_inacc:
+        print_report_line(txt=TOTAL_INACC_TXT, msg=number_of_media_inacc)
+
+    number_of_media_nosize = playlist.number_of_media_nosize
+    if number_of_media_nosize:
+        print_report_line(txt=TOTAL_NO_SIZE_TXT, msg=number_of_media_nosize, err=True)
 
 
 def cli():
@@ -186,15 +312,17 @@ def cli():
     parser.add_argument(
         "-f",
         "--format-filter",
-        type=str,
         default=DEFAULT_FORMAT,
         help="Custom format filter. See {} for details. The default is {}".format(FORMAT_DOC_URL, DEFAULT_FORMAT),
+    )
+    parser.add_argument(
+        "-m", "--more-info", action="store_true", help="Display more info on each media file (if available)."
     )
     args = parser.parse_args()
     err_msg = None
 
     try:
-        get_totalsize(args.url, args.format_filter, report_mode=True)
+        print_report(args.url, args.format_filter, more_info=args.more_info)
     except ResourceNotFoundError:
         err_msg = "Resource not found."
     except FormatSelectionError:
